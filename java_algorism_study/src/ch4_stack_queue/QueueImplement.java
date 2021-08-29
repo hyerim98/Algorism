383ba8ab7ddbcc2a74684435728a36f4b0e68b90
hyerim98 <choihyerim252@gmail.com>
2021-08-29 21:10:50 +0900

    ch4 stack, queue

---
+++ java_algorism_study/src/ch4_stack_queue/QueueImplement.java
@@ -0,0 +1,10 @@
+package ch4_stack_queue;
+
+public class QueueImplement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ java_algorism_study/src/ch4_stack_queue/StackImplement.java
@@ -0,0 +1,27 @@
+package ch4_stack_queue;
+
+class Stack{
+	
+}
+
+public class StackImplement {
+
+	public static void main(String[] args) {
+		/*Stack stack = new Stack(5);
+		
+		System.out.println(stack.push(3));
+		System.out.println(stack.push(2));
+		System.out.println(stack.push(1));
+		
+		stack.dump();
+		
+		System.out.println(stack.indexOf(8) + "에 존재합니다");
+		
+		System.out.println(stack.peek());
+		System.out.println(stack.pop());
+		
+		stack.dump();*/
+
+	}
+
+}

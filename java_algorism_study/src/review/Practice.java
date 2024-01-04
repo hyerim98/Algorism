226a2d683efc5a36c29193c39611b49f37f2d91e
hyerim98 <choihyerim252@gmail.com>
2024-01-04 18:10:19 +0900

    알고리즘 검색 연습 테스트

--- java_algorism_study/src/review/Practice.java
+++ java_algorism_study/src/review/Practice.java
@@ -7,77 +7,50 @@ import java.util.TreeSet;
 
 
 public class Practice {
-	 static final int VMAX = 21;        // 시력의 분포(0.0부터 0.1 단위로 21개)
-
-	    static class PhyscData {
-	        String name;            // 이름
-	        int    height;          // 키
-	        double vision;          // 시력
-
-	        //--- 생성자(constructor) ---//
-	        PhyscData(String name, int height, double vision) {
-	            this.name     = name;
-	            this.height = height;
-	            this.vision = vision;
+	public static int searchKey(int[] arr, int n, int key) {
+		int pl = 0;
+		int pc = n / 2;
+		int pr = n - 1;
+		
+		
+		do {		
+			if(arr[pc] == key) {
+				if(pc == 0) {
+					return pc;
+				}
+				
+				do {
+					pc--;
+				}while(arr[pc] == key);
+				
+				return ++pc;
+			}
+			else if(arr[pc] < key) {
+				pl = pc + 1;
+				pc = (pl + pr) / 2;
+			}
+			else {
+				pr = pc - 1;
+				pc = (pl + pr) / 2;
+			}
+		}while(pl <= pr);
+		
+		return -1;
+	}
+	 
+	    public static void main(String[] args) {
+	        Scanner scan = new Scanner(System.in);
+	        int[] arr = {1, 3, 5, 7, 7, 7, 7, 8, 8, 9, 9};
+	        System.out.print("검색할 값 : ");
+	        int key = scan.nextInt();
+	        
+	        int idx = searchKey(arr, arr.length, key);
+	        if(idx == -1) {
+	        	System.out.println("존재하지 않는 값 입니다");
+	        }
+	        else {
+	        	System.out.printf("arr[%d]에 존재\n", idx);
 	        }
 	    }
-
-	    //--- 키의 평균값을 구함 ---//
-	    static double aveHeight(PhyscData[] dat) {
-	        double sum = 0;
-
-	        for (int i = 0; i < dat.length; i++)
-	            sum += dat[i].height;
-
-	        return sum / dat.length;
-	    }
-
-	    //--- 시력의 분포를 구함 ---//
-	    static void distVision(PhyscData[] dat, int[] dist) {
-	        int i = 0;
-
-	        dist[i] = 0;
-	        for (i = 0; i < dat.length; i++)
-	            if (dat[i].vision >= 0.0 && dat[i].vision <= VMAX / 10.0)
-	                dist[(int)(dat[i].vision * 10)]++;
-	    }
-
-	    public static void main(String[] args) {
-	        Scanner stdIn = new Scanner(System.in);
-
-	        PhyscData[] x = {
-	            new PhyscData("강민하", 162, 0.3),
-	            new PhyscData("김찬우", 173, 0.7),
-	            new PhyscData("박준서", 175, 2.0),
-	            new PhyscData("유서범", 171, 1.5),
-	            new PhyscData("이수연", 168, 0.4),
-	            new PhyscData("장경오", 174, 1.2),
-	            new PhyscData("황지안", 169, 0.8),
-	        };
-	        int[] vdist = new int[VMAX];                    // 시력의 분포
-
-	        System.out.println("■ 신체검사 리스트 ■");
-	        System.out.println(" 이름    키   시력");
-	        System.out.println("--------------------");
-	        for (int i = 0; i < x.length; i++)
-	            System.out.printf("%-6s%3d%5.1f\n",
-	                                    x[i].name, x[i].height, x[i].vision);
-
-	        System.out.printf("\n평균 키: %5.1fcm\n", aveHeight(x));
-
-	        distVision(x, vdist);                                    // 시력의 분포를 구함
-
-	        System.out.println("\n시력 분포");
-	        for (int i = 0; i < VMAX; i++) {
-	        	System.out.printf("%3.1f～: ", i / 10.0);
-	        	for(int j = 0; j < vdist[i]; j++) {
-	        		System.out.printf("*");
-	        	}
-	        	System.out.println();
-	        } 
-	    }
 	
-
-	
-
 }
